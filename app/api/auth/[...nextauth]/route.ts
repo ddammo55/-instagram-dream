38c0b7f4ccdc3a6f408e834a0c1a5dd5f71e86fd
ddammo55 <ddammo55@naver.com>
2023-08-31 06:13:56 +0900

    현재페이지로그인

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,14 +2,19 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 
-const handler = NextAuth({
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_OAUTH_ID || '',
       clientSecret: process.env.GOOGLE_OAUTH_SECRET || '',
     }),
   ],
-});
+  // 로그인 성공시 이동할 페이지
+  pages: {
+    signIn: '/auth/signin',
+  },
+};
 
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, authOptions };
--- components/ui/ColorButton.tsx
+++ components/ui/ColorButton.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 type Props = {
-    text : string,
-    onClick : () => void
+    text : string;
+    onClick : () => void;
+    size? : 'small' | 'large';
+
 }
 
-export default function ColorButton({text,onClick} : Props) {
-    return <div className="rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.15rem]"  onClick={onClick}>
-    <button className="bg-white rounded-sm text-base p-[0.3rem] hover:opacity-90 transition-opacity " onClick={onClick}>{text}</button>
+export default function ColorButton({text,onClick, size = 'small'} : Props) {
+    return <div className={`rounded-md bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.15rem] ${size === 'large' ? 'p-[0.3rem]' : 'p-[0.15rem]' }`}>
+    <button className={`bg-white rounded-sm text-base hover:opacity-90 transition-opacity ${size === 'large' ? 'p-4 text-2xl' : 'p-[0.3rem]  text-base'}`} onClick={onClick}>{text}</button>
     {/* 버튼을 클릭하면 전달받은 onClick를 보여준다 */}
     {/* 전달받은 텍스트를 보여준다 */}
 </div>
