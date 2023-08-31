0edbebe76f23ba4db5fe884cc86436236ff0b63c
ddammo55 <ddammo55@naver.com>
2023-09-01 04:53:04 +0900

    이미지엑스박스잇슈

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -6,7 +6,8 @@ type Props = {
 export default function Avatar({image} :Props) {
     return <div className='w-9 h-9 rounded-full bg-gradient-to-bl from-fuchsia-600 via-rose-500 to-amber-300 p-[0.15rem]'>
         {/* eslint-disable-next-line @next/next/no-img-element  */}
-        <img className='rounded-full p-[0.1rem]' alt="user profile" src={image ?? undefined} />
+        <img className='rounded-full p-[0.1rem]' alt="user profile" src={image ?? undefined}  referrerPolicy="no-referrer" />
+       
     </div>
 }
 
